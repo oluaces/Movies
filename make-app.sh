98dab53e35b996b3e0dbc2a0132e8c04c98acf50
Oscar Luaces <oluaces@uniovi.es>
2023-05-22 10:50:57 +0200

    -

--- make-app.sh
+++ make-app.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-pyinstaller --icon movielens.ico --windowed --add-data "datos:datos"  movielens.py
-=======
 pyinstaller --icon movielens.png --windowed --add-data "datos:datos"  movielens.py
->>>>>>> f140ef9371be41850f418f0a89377db8b8bb38f5
